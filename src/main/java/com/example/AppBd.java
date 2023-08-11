626d00f5f1a938e05dd9766ca8848a2fdc7b3028
Danielle Sena <danielledsaraujo@gmail.com>
2023-08-11 15:16:38 +0000

    Refactor - Princípio de Separação de Preocupações

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -1,21 +1,24 @@
 package com.example;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Connection;
+
+import com.example.dao.ConnectionManager;
+import com.example.dao.EstadoDAO;
+import com.example.dao.ProdutoDAO;
+import com.example.dao.DAO;
+import com.example.model.Marca;
+import com.example.model.Produto;
+
 
 public class AppBd {
-    private static final String JDBC_URL = "jdbc:postgresql://localhost/postgres";
-    private static final String USERNAME = "gitpod";
-    private static final String PASSWORD = "";
-
     public static void main(String[] args){
         new AppBd();
     }
 
     public AppBd() {
-        try (var conn = getConnection()) {
-            listarEstados(conn);
-            localizarEstado(conn, "CE");
+        try (var conn = ConnectionManager.getConnection()) {
+            var estadoDAO = new EstadoDAO(conn);
+            estadoDAO.listar();
+            estadoDAO.localizar("CE");
 
             var marca = new Marca();
             marca.setId(2L);
@@ -26,108 +29,15 @@ public class AppBd {
             produto.setValor(321);
             produto.setNome("Produto atualizado 2");
 
-            //inserirProduto(conn, produto);
-            atualizarProduto(conn, produto);
-            excluirProduto(conn, 202L);
-            listarDadosTabela(conn, "produto");
+            var produtoDAO = new ProdutoDAO(conn);
+            //produtoDAO.inserir(conn, produto);
+            produtoDAO.atualizar(produto);
+            produtoDAO.excluir(202L);
+
+            var dao = new DAO(conn);
+            dao.listar("produto");
         } catch(SQLException e) {
             System.err.println("Não foi possível se conectar ao banco de dados: " + e.getMessage()); 
         } 
-    }
-
-    private void excluirProduto(Connection conn, long id) {
-        var sql = "delete from produto where id = ?";
-        try {
-            var statement = conn.prepareStatement(sql);
-            statement.setLong(1, id);
-            if (statement.executeUpdate() == 1)
-                System.out.println("Produto excluído com sucesso");
-            else System.out.println("Produto não foi localizado");
-        } catch (SQLException e) {
-            System.err.println("Erro ao excluir o produto: " + e.getMessage());
-        }
-    }
-
-    private void inserirProduto(Connection conn, Produto produto) {
-        var sql = "insert into produto (nome, marca_id, valor) values (?, ?, ?)";
-        try (var statement = conn.prepareStatement(sql)) {
-            statement.setString(1, produto.getNome());
-            statement.setLong(2, produto.getMarca().getId());
-            statement.setDouble(3, produto.getValor());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println("Erro ao inserir o produto: " + e.getMessage());
-        }
-    }
-
-    private void atualizarProduto(Connection conn, Produto produto) {
-        var sql = "update produto set nome = ?, marca_id = ?, valor = ? where id = ?";
-        try (var statement = conn.prepareStatement(sql)) {
-            statement.setString(1, produto.getNome());
-            statement.setLong(2, produto.getMarca().getId());
-            statement.setDouble(3, produto.getValor());
-            statement.setLong(4, produto.getId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println("Erro ao atualizar o produto: " + e.getMessage());
-        }
-    }
-
-    private void listarDadosTabela(Connection conn, String tabela) {
-        var sql = "select * from " + tabela;
-        try {
-            var statement = conn.createStatement();
-            var result = statement.executeQuery(sql);
-
-            var metadata = result.getMetaData();
-            int cols = metadata.getColumnCount();
-
-            for (int i = 1; i <= cols; i++) {
-                System.out.printf("%-25s | ", metadata.getColumnName(i));
-            }
-            System.out.println();
-
-            while (result.next()) {
-                for (int i = 1; i <= cols; i++) {
-                    System.out.printf("%-25s | ", result.getString(i));
-                }
-                System.out.println();
-            }
-        } catch (SQLException e) {
-            System.err.println("Erro na execução da consulta: " + e.getMessage());
-        }
-
-    }
-
-    private void localizarEstado(Connection conn, String uf) {
-        try {
-            var sql = "select * from estado where uf = ?";
-            var statement = conn.prepareStatement(sql);
-            statement.setString(1, uf);
-            var result = statement.executeQuery();
-            if (result.next()) {
-                System.out.printf("Id: %d Nome: %s UF: %s\n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
-            }
-        } catch (SQLException e) {
-            System.err.println("Erro ao executar consulta SQL: " + e.getMessage());
-        }
-    }
-
-    private void listarEstados(Connection conn) {
-        try {
-            System.out.println("Conexão com o banco realizada com sucesso.");
-
-            var statement = conn.createStatement();
-            var result = statement.executeQuery("select * from estado");
-            while(result.next()) {
-                System.out.printf("Id: %d Nome: %s UF: %s\n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
-            }
-        } catch(SQLException e) {
-            System.err.println("Não foi possível executar a consulta ao banco" + e.getMessage());
-        } 
-    }
-
-    private Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
     }
 }
