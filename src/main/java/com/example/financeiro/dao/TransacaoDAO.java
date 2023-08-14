1137766c13de95140d2b1fcdb9a27f51d498290a
Danielle Sena <danielledsaraujo@gmail.com>
2023-08-14 00:56:37 +0000

    Sistema financeiro (questões 1 e 2)

---
+++ src/main/java/com/example/financeiro/dao/TransacaoDAO.java
@@ -0,0 +1,14 @@
+package com.example.financeiro.dao;
+
+import java.sql.Connection;
+
+import com.example.dao.DAO;
+
+public class TransacaoDAO extends DAO {
+
+    public TransacaoDAO(Connection conn) {
+        super(conn);
+    }
+    
+    
+}
